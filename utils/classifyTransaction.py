40b0df5d3a56650477b9bec87db26f588cbf4a35
Alanson <acstumler@outlook.com>
2025-07-28 20:29:03 -0400

    Fix: Remove keyword logic from classifyTransaction, default to Uncategorized

--- utils/classifyTransaction.py
+++ utils/classifyTransaction.py
@@ -1,19 +1,4 @@
-from utils.clean_vendor_name import clean_vendor_name
-
-def classifyTransaction(data):
-    if isinstance(data, str):
-        memo = clean_vendor_name(data)
-        amount = None
-    elif isinstance(data, tuple):
-        memo = clean_vendor_name(data[0])
-        amount = data[1]
-    elif isinstance(data, dict):
-        memo = clean_vendor_name(data.get("memo", ""))
-        amount = data.get("amount", None)
-    else:
-        memo = "Unknown"
-        amount = None
-
+def classifyTransaction(memo, amount=None):
     return {
         "classification": "7090 - Uncategorized Expense"
     }
